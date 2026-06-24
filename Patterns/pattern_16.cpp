156f6c1544924eeae98640eacd3b51779a7086ed
Daniya_Ali <alidaniya419@gmail.com>
2026-06-24 21:21:51 +0500

    Add files via upload

---
+++ Patterns/pattern_16.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void pattern16(int n){
+    for(int i=0;i<n;i++){
+        char ch='A' + i;
+       for(int j=0 ; j<=i ;j++){    
+        cout<<ch<<" ";
+       
+       }
+       
+        cout<<endl;
+    }
+}
+int main(){
+    int n=5;
+    pattern16(n);
+    return 0;
+}
