fecd3e62609831e97cd2ec139a01857c12b3284c
Daniya_Ali <alidaniya419@gmail.com>
2026-06-30 21:46:34 +0500

    Add files via upload

---
+++ Hashing/Array/lower_case_hash.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+
+    //FOR LOWER CASE ONLY 
+
+    string s;
+    cout<<"Enter the string : ";
+    cin>>s;
+    cout<<endl;
+
+    //hashing 
+
+    //1.pre-storing
+    int hash[26]={0};
+    for(int i=0;i<s.size();i++){
+        //2.mapping
+        hash[s[i]-'a']++;
+    }
+
+    int q;
+    cout<<"Enter the number of characters to find their frequency : ";
+    cin>>q;
+
+    //3.fetching
+    while(q!=0){
+        char ch;
+        cout<<"Enter the character : ";
+        cin>>ch;
+        cout<<"The character occured : "<<hash[ch - 'a']<<" times."<<endl;
+        q--;
+    }
+
+
+    return 0;
+
+}
