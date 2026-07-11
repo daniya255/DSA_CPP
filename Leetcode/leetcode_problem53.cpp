e31442fc6623673320a97c4af5b6d05691b4378d
Daniya_Ali <alidaniya419@gmail.com>
2026-07-11 08:18:51 -0700

    Add files via upload

---
+++ Leetcode/leetcode_problem53.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        int maxlen = INT_MIN;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            maxlen = max(maxlen, sum);
+            if (sum < 0)
+                sum = 0;
+        }
+
+        return maxlen;
+    }
+};
