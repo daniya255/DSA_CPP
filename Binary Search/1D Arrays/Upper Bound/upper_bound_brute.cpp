87ccc9c98914216673928bbc0ce5c3a6bc16cefd
Daniya_Ali <alidaniya419@gmail.com>
2026-07-20 20:02:54 +0500

    Add files via upload

---
+++ Binary Search/1D Arrays/Upper Bound/upper_bound_brute.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int lower_bound(vector<int>&nums,int x){
+    int n=nums.size();
+    
+    //assume array is already sorted
+    for(int i=0;i<n;i++){
+        if(nums[i]>x) return i;
+    }
+    return n;
+}
+int main(){
+    vector<int>nums={3,5,8,9,15,19};
+    int x=9;
+    cout<<"The lower bound of "<<x<<" is : "<<lower_bound(nums,x);
+    return 0;
+}
+
+//Time Complexity : O(N) where N is the size of given array, as linear search is used for searching the index.
+//Space Complexity : O(1) const space is required for variables.
