344c12747159208b0e25d4c9f52347fa2603071e
Daniya_Ali <alidaniya419@gmail.com>
2026-08-04 20:31:31 +0500

    Add files via upload

---
+++ Strings/Easy/Largest Odd Number in a String/largest_odd_number.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+string largestOddNumber(string num)
+{
+
+    if (num.empty())
+        return "";
+
+    int start = 0, end = -1;
+
+    for (int i = num.size() - 1; i >= 0; i--)
+    {
+        if ((num[i] - '0') % 2 == 1)
+        {
+            end = i;
+            break;
+        }
+    }
+
+    for (int i = 0; i < end; i++)
+    {
+        if (num[i] != '0')
+            break;
+
+        start++;
+    }
+
+    return num.substr(start, end - start + 1);
+}
+int main()
+{
+    string num = "504";
+    string result = largestOddNumber(num);
+    cout << "Largest odd number: " << result << endl;
+    return 0;
+}
+
+//Time Complexity: O(N), since the loop runs once through the string of length N.Extracting the substring with .substr()takes time proportional to the length of the resulting string, which is at most O(N).
+//Space Complexity: O(1), as we are using only a constant amount of extra space.
