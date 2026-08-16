658f3e1b283b02a290bb8cc7fd776f06c295179d
Daniya_Ali <alidaniya419@gmail.com>
2026-08-16 20:52:44 +0500

    Add files via upload

---
+++ Linked List/Singly LL/Easy/Middle of the LL/middle_of_the_LL_brute.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Node class to represent each node in the linked list
+class Node{
+    public:
+        int data;
+        Node* next; // Pointer to the next node
+
+    // Constructor with data and next pointer
+    Node(int data, Node* next){
+        this->data=data;
+        this->next=next;
+    }
+
+     // Constructor with only data
+    Node(int data){
+        this->data=data;
+        this->next=nullptr;
+    }
+
+};
+
+// Function to print the linked list
+void print(Node* head){
+
+    //edge case : if linked list is empty
+    if (head==NULL) return;
+    
+    Node*temp= head;
+
+    //traverse till the tail of the linked list to connect nodes together 
+    while(temp!=nullptr){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
+
+//converts an arr/vector to a linked list and return a pointer to the head
+Node* convertArr2LL(vector<int>arr){
+
+    if (arr.empty()) return nullptr;
+
+    Node* head=new Node(arr[0]);
+    Node* temp=head;
+
+    for(int i=1;i<arr.size();i++){
+        temp->next= new Node(arr[i]);
+        temp = temp->next;
+    }
+
+    return head;
+
+}
+
+Node* middleNode(Node* head) {
+    // If the list is empty or has only
+    // one element, return the head asit's the middle.
+    if(head==nullptr || head->next==nullptr)   return head;
+
+    int count=0;
+    Node* temp=head;
+    
+    // Count the number of nodes in the linked list.
+    while(temp!=nullptr){
+        count++;
+        temp=temp->next;
+    }
+
+    // Calculate the position of the middle node.
+    int midnode=(count/2)+1;
+    temp=head;
+
+    // Traverse to the middle node by moving
+    // temp to the middle position.
+    while(temp!=nullptr){
+        midnode--;
+        
+        // Check if the middle
+        // position is reached.
+        if(midnode==0) break;   // break out of the loop to return temp
+        temp=temp->next;
+    }
+
+    return temp;
+
+    }
+
+int main(){
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+
+    // Find the middle node
+    Node* middledode = middleNode(head);
+
+    // Display the value of the middle node
+    cout << "The middle node value is: " << middledode->data << endl;
+    return 0;
+}
+
+/*
+Time Complexity: O(N+N/2) The code traverses the entire linked list once and half times and then only half in the second iteration, first to count the number of nodes then then again to get to the middle node. Therefore, the time complexity is linear, O(N + N/2) ~ O(N).
+Space Complexity : O(1) There is constant space complexity because it uses a constant amount of extra space regardless of the size of the linked list. We only use a few variables to keep track of the middle position and traverse the list, and the memory required for these variables does not depend on the size of the list.
+*/
