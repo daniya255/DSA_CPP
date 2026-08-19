a1f7c27e53c4a74627452cc5ed5647322b1f94ed
Daniya_Ali <alidaniya419@gmail.com>
2026-08-19 19:39:33 +0500

    Add files via upload

---
+++ Leetcode/Linked List/Medium/leetcode_problem328.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        
+        if (head == nullptr || head->next == nullptr)
+            return head;
+
+        ListNode* even = head->next;
+        ListNode* odd = head;
+        ListNode* even_head = even;
+
+        while (even != nullptr && even->next != nullptr) {
+            odd->next = odd->next->next;
+            odd = odd->next;
+
+            even->next = even->next->next;
+            even = even->next;
+        }
+
+        odd->next = even_head;
+
+        return head;
+    }
+};
