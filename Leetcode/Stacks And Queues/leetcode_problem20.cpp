ccfe5ba16d79a7278933ceee95f7bad99fe271a5
Daniya_Ali <alidaniya419@gmail.com>
2026-09-05 21:51:23 +0500

    Add files via upload

---
+++ Leetcode/Stacks And Queues/leetcode_problem20.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isValid(string s) {
+         if (s.empty())
+        return true;
+    if (s.size() == 1)
+        return false;
+
+    stack<char> ch;
+    for (int i = 0; i < s.size(); i++)
+    {
+
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+        {
+            ch.push(s[i]);
+        }
+        else
+        {
+            if (ch.empty())
+                return false;
+
+            char last = ch.top();
+            ch.pop();
+
+            if ((last == '(' && s[i] == ')') || (last == '{' && s[i] == '}') || (last == '[' && s[i] == ']'))
+                continue;
+            else
+                return false;
+        }
+    }
+
+    return ch.empty();
+    }
+};
