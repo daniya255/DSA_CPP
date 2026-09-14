20fb4b332aefb4a02805375eb65488d5e77db9a1
Daniya_Ali <alidaniya419@gmail.com>
2026-09-14 20:51:08 +0500

    Enhance comments and explanations in sum_of_mins_optimal.cpp
    Added comments to clarify the logic for finding previous and next smaller elements, as well as the overall contribution calculation. Updated time and space complexity explanations for better understanding.

--- Stacks And Queues/Monotonic/Sum Of Subarray Minimums/sum_of_mins_optimal.cpp
+++ Stacks And Queues/Monotonic/Sum Of Subarray Minimums/sum_of_mins_optimal.cpp
@@ -9,27 +9,37 @@ int sumMins(vector<int>&nums){
     long long totalSum = 0;
     int MOD = 1e9 + 7;
 
+    //finding PSE(Previous Smaller Element)
     for(int i=0;i<n;i++){
+        //uisng stack to check if there is a smaller element before or not
         while(!st.empty() && nums[st.top()] > nums[i]){
             st.pop();
         }
+        //check if current is smallest or an other element before it
         left[i]= (st.empty()) ? -1 : st.top();
         st.push(i);
     }
 
+    //emptying stack for NSE
     while(!st.empty()) st.pop();
 
+    //finding NSE from right to left
     for(int i=n-1;i>=0;i--){
+        //checking for NSE by comparing current element with the elements in stack
         while(!st.empty() && nums[st.top()] >= nums[i]){
             st.pop();
         }
+        //check if there is a NSE or not, if not the current element can stretch till end
         right[i]= st.empty() ? n : st.top();
         st.push(i);
     }
 
     for(int i=0; i<n; i++){
+        //finding left boundary
         long long leftCount= i - left[i];
+        //finding right boundary
         long long rightCount= right[i] - i;
+        //finding total subarrays and multiplying them to the value
         long long contribution = (nums[i] * leftCount % MOD) * rightCount % MOD;
         totalSum = (totalSum + contribution) % MOD;
     }
