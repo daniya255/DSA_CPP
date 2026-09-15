8300a5a7bbdac22e3c6235d4dd19547311a566f2
Daniya_Ali <alidaniya419@gmail.com>
2026-09-15 19:40:46 +0500

    Refactor nextGreaterElement function and main
    Refactored the nextGreaterElement function and main function for clarity and consistency. Improved formatting and comments for better understanding.

--- Stacks And Queues/Monotonic/Next Greater Element/next_greater_element_optimal.cpp
+++ Stacks And Queues/Monotonic/Next Greater Element/next_greater_element_optimal.cpp
@@ -32,7 +32,7 @@ vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
 
     return result;
 }
-
+//main function
 int main() {
     vector<int> nums1 = {4, 1, 2};
     vector<int> nums2 = {1, 3, 4, 2};
